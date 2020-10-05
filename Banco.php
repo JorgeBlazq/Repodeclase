4ff5535bd71f513c166b1c95296b97df1dd507e8
JorgeBlazq <71818217+JorgeBlazq@users.noreply.github.com>
2020-10-05 10:01:32 +0200

    Add files via upload

--- Banco.php
+++ Banco.php
@@ -24,8 +24,8 @@ switch ($resto) {
 		break;
 }
 $resto=(((int)substr($ncuenta, 0, 1)*1)+((int)substr($ncuenta, 1, 1)*2)+((int)substr($ncuenta, 2, 1)*4)+((int)substr($ncuenta, 3, 1)*8)+((int)substr($ncuenta, 4, 1)*5)+((int)substr($ncuenta, 5, 1)*10)+((int)substr($ncuenta, 6, 1)*9)+((int)substr($ncuenta, 7, 1)*7)+((int)substr($ncuenta, 8, 1)*3)+((int)substr($ncuenta, 9, 1)*6))%11;
-$resto2=11-$resto;
-switch ($resto2) {
+$resto=11-$resto;
+switch ($resto) {
 	case '11';
 		$dc1="0";
 		break;
@@ -34,7 +34,7 @@ switch ($resto2) {
 		break;
 	
 	default:
-		$dc1=strval($resto2);
+		$dc1=strval($resto);
 		break;
 }
 
