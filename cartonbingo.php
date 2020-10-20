2669e07edb4a2ba62a9493770b337dd5f0e6817c
JorgeBlazq <71818217+JorgeBlazq@users.noreply.github.com>
2020-10-20 11:57:21 +0200

    Add files via upload

--- cartonbingo.php
+++ cartonbingo.php
@@ -6,33 +6,40 @@
 
 <?php 
 $jugadores = array(
-1 => array(1 => array(), 2 => array(), 3 => array()),
-2 => array(1 => array(), 2 => array(), 3 => array()),
-3 => array(1 => array(), 2 => array(), 3 => array()),
-4 => array(1 => array(), 2 => array(), 3 => array())
-);
+array(array(),array(),array()),
+array(array(),array(),array()),
+array(array(),array(),array()),
+array(array(),array(),array())
+);//Array de 4 jugadores con 3 arrays de carton cada uno
+
+$jugadoresog=$jugadores;//Para recordar los numeros de los cartones y esas cosas
 
 $bombo = range(1,60);
-$contjugador=1;
-$contcarton=1;
+$contjugador=0;
+$contcarton=0;
 $ganador=false;
+
 $bola;
-while ($contjugador <= 4) {
-			while ($contcarton<=3) {
+
+
+
+while ($contjugador < 4) {
+			while ($contcarton<3) {
 				$copiabombo=$bombo;
+				shuffle($copiabombo);
 				while (count($jugadores[$contjugador][$contcarton])<15) {
-					array_push($jugadores[$contjugador][$contcarton], $copiabombo[rand(0,(count($bombo)-1))]);
+					array_push($jugadores[$contjugador][$contcarton],array_pop($copiabombo));
 				}
 				$contcarton++;
 			}
-			$contcarton=1;
+			$contcarton=0;
 			$contjugador++;
 	}
 	shuffle($bombo);
 while ($ganador==false) {
 	$bola=array_pop($bombo);
-	for ($i=1; $i <=4 ; $i++) { 
-		for ($j=1; $j <=3 ; $j++) { 
+	for ($i=0; $i <4 ; $i++) { 
+		for ($j=0; $j <3 ; $j++) { 
 			if (in_array($bola, $jugadores[$i][$j])) {
 				array_splice($jugadores[$i][$j], array_search($bola, $jugadores[$i][$j]),1);
 			}
@@ -40,12 +47,13 @@ while ($ganador==false) {
 				$ganador=true;
 				$contjugador=$i;
 				$contcarton=$j;
+				echo "</br> Ha ganado el jugador ".(intval($contjugador)+1)." con el carton ".(intval($contcarton)+1).".";
 			}
 		}
 	}
 
 }
-echo "</br> Ha ganado el jugador ".$contjugador." con el carton ".$contcarton.".";
+
 
 //JORGE BLAZQUEZ
 ?>
