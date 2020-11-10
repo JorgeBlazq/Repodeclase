4c1def9ae3fd9d645f16cad6866c27bd38b82199
JorgeBlazq <71818217+JorgeBlazq@users.noreply.github.com>
2020-11-10 13:06:40 +0100

    Add files via upload

--- forms/datos/datos.php
+++ forms/datos/datos.php
@@ -1,7 +1,10 @@
 <?php
  echo "<h1>Datos Alumnos</h1>";
  echo "<table style='border: 1px solid black;'><tr style='border: 1px solid black;'><td style='border: 1px solid black;'>Nombre</td><td style='border: 1px solid black;'>Apellidos<td style='border: 1px solid black;'>Email</td><td style='border: 1px solid black;'>Genero</td></tr><tr style='border: 1px solid black;'><td style='border: 1px solid black;'>".$_POST['nombre']."</td><td style='border: 1px solid black;'>".$_POST['ape1']." ".$_POST['ape2']."</td><td style='border: 1px solid black;'>".$_POST['email']."</td><td style='border: 1px solid black;'>".$_POST['genero']."</td></tr></table>";
+if (($_POST['nombre']!="") && ($_POST['email']!="") && ($_POST['genero']!="")) {
+	# code...
 
 $f1=fopen("datos.txt","a");
 fwrite($f1, $_POST['nombre']."###".$_POST['ape1']." ".$_POST['ape2']."###".strtolower($_POST['email'])."###".$_POST['genero']."\r\n");
+}else//mandar un error
 ?>
